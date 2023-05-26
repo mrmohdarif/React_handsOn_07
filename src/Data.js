499e2787a53818a1d5e86209368bfc2603ae7c2f
mohd arif <alliswell5solution@gmail.com>
2023-05-26 22:50:34 +0530

    0.0

---
+++ src/Data.js
@@ -0,0 +1,10 @@
+const Student=[     
+{"id": 0, "Name": "Mohd arif",  "Age": "18",  "Course": "MEAN", "Batch": "Jan" },  
+{"id": 1, "Name": "Mr Boney",   "Age": "19",  "Course": "MERN", "Batch": "Fab" },    
+{"id": 2, "Name": "Joney", "Age": "20",  "Course": "MERN", "Batch": "Mar" },     
+{"id": 3, "Name": "Sunny", "Age": "21",  "Course": "MEAN", "Batch": "Sep" },     
+{"id": 4, "Name": "Zukarbarg","Age": "22",  "Course": "MEV", "Batch": "Oct" },    
+{"id": 5, "Name": "Eliyana", "Age": "23",  "Course": "MERN", "Batch": "Nove" }, 
+]  
+         
+export default Student;
